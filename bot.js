a99c121179280a512bfd5b9d16e7989f940cd098
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2018-06-13 20:03:24 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const commando = require('discord.js-commando');
 const bot = new commando.Client();
 
 bot.registry.registerGroup('random', 'Random');
-bot.registry.registerGroup('sanss', 'Sanss');
+bot.registry.registerGroup('quacks', 'Quacks');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
