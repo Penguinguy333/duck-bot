8e015601e9b1dc2c7e55ebb571f1fb11745d99b4
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2018-06-13 19:45:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,4 @@ bot.registry.registerGroup('quacks', 'Quacks');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('Mzk3NTg3NzczMjA2NDI5Njk3.DS3nyg.JB1TOfHU5Tc91x61YFn7_9jfIOw');
+bot.login('NDU2NjQ5MDM3MDg1NTQwMzYy.DgNoDg.VNwppI38pHmo9NHqsNF3EyeRoe4');
