44a1f9fe369c660e520324465d257c33fc492ab8
Penguinguy333 <35020311+Penguinguy333@users.noreply.github.com>
2018-06-13 19:48:46 -0700

    Update quak.js

--- commands/quacks/quak.js
+++ commands/quacks/quak.js
@@ -3,16 +3,16 @@ const commando = require('discord.js-commando');
 class Quak extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'quak',
-            group: 'quacks',
-            memberName: 'quak',
-            description: 'quaks'
+            name: 'san',
+            group: 'sanss',
+            memberName: 'san',
+            description: 'sans'
         });
     }
 
     async run(message, args) {
         var roll = Math.floor(Math.random() * 6) + 1;
-        message.reply("quak");
+        message.reply("i dont like black people");
     }
 }
 
